565dda1c6efb024d484da471e996d11f25a1f086
albatron34000 <82473077+albatron34000@users.noreply.github.com>
2024-03-28 23:25:34 +0100

    Update and rename twrp_RMX2185.mk to twrp_RMX2151.mk

---
+++ twrp_RMX2151.mk
@@ -0,0 +1,57 @@
+#
+# Copyright (C) 2021 Team Win Recovery Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+# Release name
+PRODUCT_RELEASE_NAME := RMX2151
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+LOCAL_PATH := device/realme/RMX2151
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit from begonia device
+$(call inherit-product, device/realme/RMX2151/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := RMX2151
+PRODUCT_NAME := twrp_RMX2151
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := Realme 7
+PRODUCT_MANUFACTURER := realme
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
+
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    android.hardware.fastboot@1.1-impl-mock.recovery
+
+# Additional target Libraries
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libpuresoftkeymasterdevice
+
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
