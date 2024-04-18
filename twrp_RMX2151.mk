8b869e44bfd24318470dadf6402b1d9122836dd8
albatron34000 <82473077+albatron34000@users.noreply.github.com>
2024-04-18 22:20:00 +0200

    Update twrp_RMX2151.mk

--- twrp_RMX2151.mk
+++ twrp_RMX2151.mk
@@ -47,8 +47,3 @@ TARGET_RECOVERY_DEVICE_MODULES += \
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
-    libpuresoftkeymasterdevice
-
-TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
