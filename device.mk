de684eadd69f13d2b5e663b9bd0dde1b700a66b1
albatron34000 <82473077+albatron34000@users.noreply.github.com>
2024-04-18 13:30:43 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -23,6 +23,10 @@ TARGET_RECOVERY_DEVICE_MODULES += \
     libkeymaster4 \
     libpuresoftkeymasterdevice
 
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
