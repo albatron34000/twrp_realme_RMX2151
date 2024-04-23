9c32b18836806cc4858e676cdaa406481bf64b80
albatron34000 <82473077+albatron34000@users.noreply.github.com>
2024-04-23 10:22:12 +0100

    Update twrp_RMX2151.mk

--- twrp_RMX2151.mk
+++ twrp_RMX2151.mk
@@ -39,10 +39,6 @@ PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme 7
 PRODUCT_MANUFACTURER := realme
 
-# Additional target Libraries
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libkeymaster4 \
-    libpuresoftkeymasterdevice
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
