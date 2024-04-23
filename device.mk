1dcc78ae875824caf138b80a48d84c57da64ccc9
albatron34000 <82473077+albatron34000@users.noreply.github.com>
2024-04-23 10:41:57 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -36,10 +36,10 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service
 
 PRODUCT_COPY_FILES += \
-$(OUT_DIR)/target/product/rmx2151/vendor/bin/hw/android.hardware.gatekeeper@1.0-service:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/android.hardware.gatekeeper@1.0-service
+$(OUT_DIR)/target/product/RMX2151/vendor/bin/hw/android.hardware.gatekeeper@1.0-service:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/android.hardware.gatekeeper@1.0-service
 
 PRODUCT_COPY_FILES += \
-$(OUT_DIR)/target/product/rmx2151/vendor/bin/hw/vendor.oplus.hardware.cryptoeng@1.0-service:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/hw/vendor.oplus.hardware.cryptoeng@1.0-service
+$(OUT_DIR)/target/product/RMX2151/vendor/bin/hw/vendor.oplus.hardware.cryptoeng@1.0-service:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/hw/vendor.oplus.hardware.cryptoeng@1.0-service
 
 # Additional Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
